a744d63838b837ead1efba8039156b403b54209b
FernandoOrtiz98 <141680304+FernandoOrtiz98@users.noreply.github.com>
2023-09-08 19:41:33 -0300

    Create Alumno.java

---
+++ src/universidadgrupo_34/entidades/Alumno.java
@@ -0,0 +1,88 @@
+package universidadgrupo_34.entidades;
+
+import java.time.LocalDate;
+
+
+public class Alumno {
+    private int idAlumno=-1;
+    private int dni;
+    private String apellido;
+    private String nombre;
+    private LocalDate fNacimiento;
+    private boolean estado;
+    
+     public Alumno(int idAlumno, int dni, String apellido, String nombre, LocalDate fNacimiento, boolean estado) {
+        this.idAlumno=idAlumno;
+        this.dni = dni;
+        this.apellido = apellido;
+        this.nombre = nombre;
+        this.fNacimiento = fNacimiento;
+        this.estado = estado;
+    }
+
+    public Alumno(int dni, String apellido, String nombre, LocalDate fNacimiento, boolean estado) {
+        this.idAlumno=-1;
+        this.dni = dni;
+        this.apellido = apellido;
+        this.nombre = nombre;
+        this.fNacimiento = fNacimiento;
+        this.estado = estado;
+    }
+    public Alumno(){
+        this.idAlumno=-1;
+    }
+
+    public int getIdAlumno() {
+        return idAlumno;
+    }
+
+    public int getDni() {
+        return dni;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public LocalDate getfNacimiento() {
+        return fNacimiento;
+    }
+
+    public boolean isEstado() {
+        return estado;
+    }
+
+    public void setIdAlumno(int idAlumno) {
+        this.idAlumno = idAlumno;
+    }
+
+    public void setDni(int dni) {
+        this.dni = dni;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setfNacimiento(LocalDate fNacimiento) {
+        this.fNacimiento = fNacimiento;
+    }
+
+    public void setEstado(boolean estado) {
+        this.estado = estado;
+    }
+    
+    
+    @Override
+    public String toString() {
+        return "idAlumno=" + idAlumno + ", dni=" + dni + ", apellido=" + apellido + ", nombre=" + nombre + ", estado=" + estado + fNacimiento ;
+    }
+}
