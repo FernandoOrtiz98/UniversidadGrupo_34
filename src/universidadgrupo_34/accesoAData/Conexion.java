2e2e6d7d184f4bb53d0d3ec129f3263112d44d21
FernandoOrtiz98 <141680304+FernandoOrtiz98@users.noreply.github.com>
2023-09-08 19:41:29 -0300

    Create Conexion.java
    adsa

---
+++ src/universidadgrupo_34/accesoAData/Conexion.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package universidadgrupo_34.accesoAData;
+
+/**
+ *
+ * @author Fer
+ */
+public class Conexion {
+    private static final String URL="jdbc:mariadb://Localhost/";
+    private static final String DB="universidad";
+    
+}
