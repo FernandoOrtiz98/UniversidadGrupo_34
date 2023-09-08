4c719cb0d0726cabe48d274397b300b33dd29615
FernandoOrtiz98 <141680304+FernandoOrtiz98@users.noreply.github.com>
2023-09-08 19:41:36 -0300

    Create Inscripcion.java

---
+++ src/universidadgrupo_34/entidades/Inscripcion.java
@@ -0,0 +1,36 @@
+package universidadgrupo_34.entidades;
+
+/**
+ *
+ * @author Fer
+ */
+public class Inscripcion {
+    private int idInscripcion;
+    private Alumno alumno;
+    private Materia materia;
+    private double nota;
+
+    public Inscripcion() {
+    }
+
+    public Inscripcion(Alumno alumno, Materia materia, double nota) {
+        this.alumno = alumno;
+        this.materia = materia;
+        this.nota = nota;
+    }
+
+    public Inscripcion(int idInscripcion, Alumno alumno, Materia materia, double nota) {
+        this.idInscripcion = idInscripcion;
+        this.alumno = alumno;
+        this.materia = materia;
+        this.nota = nota;
+    }
+
+    @Override
+    public String toString() {
+        String insc=idInscripcion+" "+alumno.getApellido()+", "+alumno.getNombre()+" "+materia.getNombre();
+        return insc;
+    }
+    
+    
+}
