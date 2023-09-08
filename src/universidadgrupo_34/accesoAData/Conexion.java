b87d38a080675f2d238268021fe21b19b472c771
FernandoOrtiz98 <141680304+FernandoOrtiz98@users.noreply.github.com>
2023-09-08 20:11:58 -0300

    Update Conexion.java

--- src/universidadgrupo_34/accesoAData/Conexion.java
+++ src/universidadgrupo_34/accesoAData/Conexion.java
@@ -1,6 +1,13 @@
 
 package universidadgrupo_34.accesoAData;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author Fer
@@ -10,5 +17,23 @@ public class Conexion {
     private static final String DB="universidad";
     private static final String USUARIO="root";
     private static final String PASSWORD="";
+    private static Connection conexion;
+    
+    public static Connection getConexion(){
+    
+        if(conexion==null){
+        
+            try {
+                Class.forName("org.mariadb.jdbc.Driver");
+                conexion=DriverManager.getConnection(URL+DB,USUARIO,PASSWORD);
+            } catch (ClassNotFoundException ex) {
+                JOptionPane.showMessageDialog(null,"Error al cargar los driver");
+            } catch (SQLException ex) {
+                JOptionPane.showMessageDialog(null,"Error de conexion");
+            }
+            
+        }
+        return conexion;
+    }
     
 }
