9bb4232117ef7312d43b68049db4d6bfd6d46e51
JosCapdev <josecapdevila512@gmail.com>
2023-09-11 18:50:26 -0300

    materiaData

--- src/universidadgrupo_34/accesoAData/MateriaData.java
+++ src/universidadgrupo_34/accesoAData/MateriaData.java
@@ -5,10 +5,46 @@
  */
 package universidadgrupo_34.accesoAData;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import javax.swing.JOptionPane;
+import org.mariadb.jdbc.Statement;
+import universidadgrupo_34.entidades.Materia;
+
 /**
  *
  * @author Jose
  */
 public class MateriaData {
+    private Connection con=null;
+    public static ArrayList <Materia> listaMateria = new ArrayList<>();
     
+    public MateriaData(){
+    this.con = Conexion.getConexion();
+    }
+
+    public void guardarMateria(Materia mat){
+        String query="INSERT INTO materia(nombre,anio,estado) VALUES (?,?,?)";
+        try {
+            PreparedStatement ps = con.prepareStatement(query,Statement.RETURN_GENERATED_KEYS);
+                ps.setString(1, mat.getNombre());
+                ps.setInt(2, mat.getAnio());
+                ps.setBoolean(3,mat.isEstado());
+                ps.executeUpdate();
+                
+            ResultSet rs = ps.getGeneratedKeys();
+            if(rs.next()){
+                mat.setIdMateria(rs.getInt(1));
+            }else{
+                JOptionPane.showMessageDialog(null,"No se pudo tener el ID...");
+            }
+            ps.close();
+            JOptionPane.showMessageDialog(null,"Guardado!");
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null,"Error de conexion... "+ex.getMessage());
+        }
+    }
 }
