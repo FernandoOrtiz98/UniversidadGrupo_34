03659c0ce795bdd9f7888450c870d2f427a80b2a
FernandoOrtiz98 <141680304+FernandoOrtiz98@users.noreply.github.com>
2023-09-11 20:26:01 -0300

    Update MateriaData.java

--- src/universidadgrupo_34/accesoAData/MateriaData.java
+++ src/universidadgrupo_34/accesoAData/MateriaData.java
@@ -6,12 +6,14 @@
 package universidadgrupo_34.accesoAData;
 
 import java.sql.Connection;
+import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import javax.swing.JOptionPane;
 import org.mariadb.jdbc.Statement;
+import universidadgrupo_34.entidades.Alumno;
 import universidadgrupo_34.entidades.Materia;
 
 /**
@@ -47,4 +49,60 @@ public class MateriaData {
             JOptionPane.showMessageDialog(null,"Error de conexion... "+ex.getMessage());
         }
     }
+    public void modificarMateria(Materia mat){
+        String query="UPDATE materia SET nombre=?,anio=? ,estado=?"
+                + "where idMateria=?";
+        try {
+            PreparedStatement ps= con.prepareStatement(query);
+               ps.setString(1, mat.getNombre());
+                ps.setInt(2, mat.getAnio());
+                ps.setBoolean(3,mat.isEstado());
+                ps.executeUpdate();
+                int exito=ps.executeUpdate();
+                if(exito==1){
+                    JOptionPane.showMessageDialog(null, "materia Modificada");
+                }
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null,"Error de conexion... "+ex.getMessage());
+        }
+    }
+    public void eliminarMateria(int id){
+        String query="UPDATE materia SET estado=0"
+                + "where idMateria=?";
+        try {
+            PreparedStatement ps= con.prepareStatement(query);
+                ps.setInt(1,id);
+                ps.executeUpdate();
+                int exito=ps.executeUpdate();
+                if(exito==1){
+                    JOptionPane.showMessageDialog(null, "materia eliminada");
+                }
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null,"Error de conexion... "+ex.getMessage());
+        }        
+    }
+    public  Materia buscarMateria(int id){
+        String sql="SELECT nombre,anio,estado FROM materia WHERE idMateria =? AND estado = 1";
+        Materia materia=null;
+        try {
+            PreparedStatement ps= con.prepareStatement(sql);
+            ps.setInt(1, id);
+            ResultSet rs=ps.executeQuery();
+            if(rs.next()){
+                materia= new Materia();
+                materia.setIdMateria(id);
+                materia.setNombre(rs.getString("nombre"));
+                materia.setAnio(rs.getInt("anio"));
+                materia.setEstado(true);
+            
+            }else{
+                JOptionPane.showMessageDialog(null, "no existe la materia");
+            }
+            ps.close();
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null,"Error al acceder a la tabla materia");
+        }
+        return materia;      
+    }
+    
 }
