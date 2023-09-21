42e8acc14b541610a9a825324aaa15039a712a55
FernandoOrtiz98 <141680304+FernandoOrtiz98@users.noreply.github.com>
2023-09-21 19:45:42 -0300

    Update UniversidadGrupo_34.java

--- src/universidadgrupo_34/UniversidadGrupo_34.java
+++ src/universidadgrupo_34/UniversidadGrupo_34.java
@@ -3,18 +3,24 @@ package universidadgrupo_34;
 
 import java.time.LocalDate;
 import universidadgrupo_34.accesoAData.AlumnoData;
+import universidadgrupo_34.accesoAData.MateriaData;
 import universidadgrupo_34.entidades.Alumno;
+import universidadgrupo_34.entidades.Materia;
 
 
 public class UniversidadGrupo_34 {
 
     
     public static void main(String[] args) {
-        Alumno alu=new Alumno(12345678,"Gonzales","juan",LocalDate.of(1999, 10, 20),true);
-        AlumnoData alum=new AlumnoData();
-        //alum.guardarAlumno(alu);
-        Alumno alumnoEncontrado=alum.buscarAlumnoDni(12345678);
-        System.out.println(alumnoEncontrado);
+//        Alumno alu=new Alumno(12345678,"Gonzales","juan",LocalDate.of(1999, 10, 20),true);
+//        AlumnoData alum=new AlumnoData();
+//        //alum.guardarAlumno(alu);
+//        Alumno alumnoEncontrado=alum.buscarAlumnoDni(12345678);
+//        System.out.println(alumnoEncontrado);
+        Materia mat = new Materia();
+        MateriaData matdat = new MateriaData();
+        mat=matdat.buscarMateria(1);
+        System.out.println("materia: "+mat.getIdMateria());
     }
     //
     
