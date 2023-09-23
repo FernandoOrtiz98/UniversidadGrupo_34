f50a9fef1098e5d326e2e8514ece81e45184e8ad
FernandoOrtiz98 <141680304+FernandoOrtiz98@users.noreply.github.com>
2023-09-23 18:21:12 -0300

    Update InscripcionData.java

--- src/universidadgrupo_34/accesoAData/InscripcionData.java
+++ src/universidadgrupo_34/accesoAData/InscripcionData.java
@@ -133,8 +133,8 @@ public class InscripcionData {
     public List<Materia> obtenerMateriasCursadas(int idAlumno){
         ArrayList<Materia> materias = new ArrayList();
         String sql="SELECT inscripcion.idMateria,nombre,anio FROM inscripcion,"
-                + "materia WHERE inscripcion.idMateria = materia.idMateria"
-                + "AND inscripcion.idAlumno = ?;";
+                + " materia WHERE inscripcion.idMateria = materia.idMateria "
+                + " AND inscripcion.idAlumno = ?;";
         try {
             PreparedStatement ps= con.prepareStatement(sql);
             ps.setInt(1,idAlumno);
@@ -156,8 +156,8 @@ public class InscripcionData {
     }
     public List<Materia> obtenerMateriasNoCursadas(int idAlumno){
         ArrayList<Materia> materias = new ArrayList();
-        String sql="SELECT * FROM materia WHERE estado = 1 AND idMateria not in"
-                + "(SELECT idMateria FROM inscripcion WHERE idAlumno = ?)";
+        String sql="SELECT * FROM materia WHERE estado = 1 AND idMateria not in "
+                + " ( SELECT idMateria FROM inscripcion WHERE idAlumno = ?);";
         try {
             PreparedStatement ps= con.prepareStatement(sql);
             ps.setInt(1,idAlumno);
