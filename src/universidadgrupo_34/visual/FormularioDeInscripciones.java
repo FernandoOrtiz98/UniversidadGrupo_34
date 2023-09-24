5e8dc5ef8033cf1a35dc527fa46644ae705d9b82
JosCapdev <josecapdevila512@gmail.com>
2023-09-24 19:07:07 -0300

    Update FormularioDeInscripciones.java

--- src/universidadgrupo_34/visual/FormularioDeInscripciones.java
+++ src/universidadgrupo_34/visual/FormularioDeInscripciones.java
@@ -231,6 +231,7 @@ public class FormularioDeInscripciones extends javax.swing.JInternalFrame {
         if(filaSeleccionada!=-1){
             Alumno a=(Alumno)jcbAlumnos.getSelectedItem();
             int idMateria=(Integer)modelo.getValueAt(filaSeleccionada, 0);
+            System.out.println("select "+a.getIdAlumno()+" "+idMateria);
             inscData.borrarInscripcionAlumnoMateria(a.getIdAlumno(),idMateria);
             borrarFilas();
         }
