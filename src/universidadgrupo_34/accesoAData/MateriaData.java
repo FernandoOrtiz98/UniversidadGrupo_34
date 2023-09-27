dc5ce70ac7ffaa6e682eefc39352a74daf8570c3
FernandoOrtiz98 <141680304+FernandoOrtiz98@users.noreply.github.com>
2023-09-27 16:53:43 -0300

    Update MateriaData.java

--- src/universidadgrupo_34/accesoAData/MateriaData.java
+++ src/universidadgrupo_34/accesoAData/MateriaData.java
@@ -1,12 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package universidadgrupo_34.accesoAData;
 
 import java.sql.Connection;
-import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -14,7 +9,6 @@ import java.util.ArrayList;
 import java.util.List;
 import javax.swing.JOptionPane;
 import org.mariadb.jdbc.Statement;
-import universidadgrupo_34.entidades.Alumno;
 import universidadgrupo_34.entidades.Materia;
 
 /**
